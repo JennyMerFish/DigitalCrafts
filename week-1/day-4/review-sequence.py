7f04bfef86502f4974458d90602b8cda68000b41
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-05 12:55:56 -0500

    update review

--- week-1/day-4/review-sequence.py
+++ week-1/day-4/review-sequence.py
@@ -1,3 +1,9 @@
-list1 =[]
-tuple1 = ()
-dogs = ["german shepherd", "bulldog", "doberman"]
+dogs = ["lab", "corgi", "bulldog", "peewee", "dachsund"]
+
+startingPoint = 5
+count = 0
+while startingPoint < 10:
+    print(dogs[count], count, startingPoint)
+    count += 1
+    startingPoint += 1
+
