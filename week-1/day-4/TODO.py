fd0b3421842473ceabe1e03b8a13ef2f2a05e19c
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-06 13:46:34 -0500

    Refactored ToDo

--- week-1/day-4/TODO.py
+++ week-1/day-4/TODO.py
@@ -1,40 +1,46 @@
 loop = True
 toDoList =[]
+def addATask():
+    addTitle = input("What is the title of the task? \n")
+    addPriority = input("What priority is this task, High, Medium, or Low?\n")
+    task = addTitle + " - " + addPriority
+    toDoList.append(task)
+    return("Here is your revised To-Do list" + str(toDoList))
+def delATask():
+    for key, value in enumerate(toDoList):
+        print(key, value)
+
+    delTask = int(input("Which task would you like to delete? \n"))
+    del toDoList[delTask]
+    return("Here is your revised To-Do list: " + str(toDoList))  
+def viewATask():
+    taskDict ={}
+    taskDict = toDoList
+    count = 1
+    for task in toDoList:
+        print(count, task)
+        count += 1
+def quitProgram():
+    return("Thanks for using the refactored version of To-Do list app, have a great day!")
+    
+
 
 while loop:
 
     ans = input("Press 1 to add task \nPress 2 to delete task \nPress 3 to view all tasks \nPress q to quit\n ")
-    
     if ans == "1":
-        addTitle = input("What is the title of the task? \n")
-        addPriority = input("What priority is this task, High, Medium, or Low?\n")
-        task = addTitle + " - " + addPriority
-        toDoList.append(task)
-        print("Here is your revised To-Do list" + str(toDoList))
-
-    
+        print(addATask())    
+        
     elif ans == "2":
-        for key, value in enumerate(toDoList):
-            print(key, value)
-
-        delTask = int(input("Which task would you like to delete? \n"))
-        del toDoList[delTask]
-
-        print("Here is your revised To-Do list: " + str(toDoList))
-    
+        print(delATask())
+         
     elif ans == "3":
-        taskDict ={}
-        taskDict = toDoList
-        count = 1
-        for task in toDoList:
-           print(count, task)
-           count += 1
-    
-
-    
+        print(viewATask())
+        
     elif ans == "q":
-        print("Thanks for using To-Do list!")
-        loop = False    
+        print(quitProgram())
+        loop = False
+        
     
     else:
         print("That is not an option, try again, DUMMY.")
