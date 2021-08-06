48a1e42532c9f32390d51563ad953a3ddca94f34
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-06 09:48:29 -0500

    toDo app

--- week-1/day-4/TODO.py
+++ week-1/day-4/TODO.py
@@ -1,35 +1,41 @@
 loop = True
-toDoList = []
-while loop:
-    ans = input("Press 1 to add task \nPress 2 to delete task \nPress 3 to view all tasks \nPress q to quit\n ")
-    if ans == "1":
-        titleList =[]
-        priorityList =[]
-        addTitle = input("What is the title of the task? \n")
-        addPriority = int(input("What priority is this task, 1 - High, 2 - Medium, or 3 - Low?\n"))
-        if addPriority == "1":
-            priority = " - High"
-        elif addPriority == "2":
-            priority = " - Medium"
-        elif addPriority == "3":
-            priority = " - Low"
-        titleList.append(addTitle)
-        priorityList.append(addPriority)
+toDoList =[]
 
-        
+while loop:
+
+    ans = input("Press 1 to add task \nPress 2 to delete task \nPress 3 to view all tasks \nPress q to quit\n ")
+    
+    if ans == "1":
+        addTitle = input("What is the title of the task? \n")
+        addPriority = input("What priority is this task, High, Medium, or Low?\n")
+        task = addTitle + " - " + addPriority
+        toDoList.append(task)
         print("Here is your revised To-Do list" + str(toDoList))
+
     
     elif ans == "2":
         for key, value in enumerate(toDoList):
             print(key, value)
+
         delTask = int(input("Which task would you like to delete? \n"))
-        
+        del toDoList[delTask]
+
         print("Here is your revised To-Do list: " + str(toDoList))
+    
     elif ans == "3":
-        print(toDoList)
+        taskDict ={}
+        taskDict = toDoList
+        count = 1
+        for task in toDoList:
+           print(count, task)
+           count += 1
+    
+
+    
     elif ans == "q":
         print("Thanks for using To-Do list!")
         loop = False    
+    
     else:
         print("That is not an option, try again, DUMMY.")
 
