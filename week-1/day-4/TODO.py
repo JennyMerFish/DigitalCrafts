98c67821bea2bcace8d4401d96febf9dcee3755a
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-06 15:23:33 -0500

    copied but I understand, i think

--- week-1/day-4/TODO.py
+++ week-1/day-4/TODO.py
@@ -5,14 +5,15 @@ def addATask():
     addPriority = input("What priority is this task, High, Medium, or Low?\n")
     task = addTitle + " - " + addPriority
     toDoList.append(task)
-    return("Here is your revised To-Do list" + str(toDoList))
+    return("Here is your To-Do list" + str(toDoList))
+    
 def delATask():
     for key, value in enumerate(toDoList):
         print(key, value)
-
     delTask = int(input("Which task would you like to delete? \n"))
     del toDoList[delTask]
     return("Here is your revised To-Do list: " + str(toDoList))  
+
 def viewATask():
     taskDict ={}
     taskDict = toDoList
@@ -20,6 +21,7 @@ def viewATask():
     for task in toDoList:
         print(count, task)
         count += 1
+
 def quitProgram():
     return("Thanks for using the refactored version of To-Do list app, have a great day!")
     
--- week-1/day-5/extraWork.py
+++ week-1/day-5/extraWork.py
@@ -2,16 +2,34 @@
 string1 = "alphabet"
 string2 = "barbados"
 string3 = "crunchy"
+string4 = "mmddgghhrr"
+NO_OF_CHARS = 256
 
-def lengthOfArray(str):
-    return len(str)
+def getCharCountArray(string):
+    count = [0] * NO_OF_CHARS
+    for i in string:
+        count[ord(i)]+= 1
+    return count
 
-def firstNonRepeatingIndex(str):
-    count = lengthOfArray(str)
-    index = 0
-    for i in str:
-        
+def firstNonRepeating(string):
+    count = getCharCountArray(string)
+    index = -1
+    k = 0
  
-    print(index)
+    for i in string:
+        if count[ord(i)] == 1:
+            index = k
+            break
+        k += 1
+ 
+    return index
 
-firstNonRepeatingIndex(string1)  
+
+index = firstNonRepeating(string4)
+
+if index == -1:
+    print("Either all characters are repeating or string is empty")
+else:
+    print("First non-repeating character is " + string4[index])
+ 
+   
