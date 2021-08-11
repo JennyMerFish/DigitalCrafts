832a2c52f3fdbf3fd7744a1d4f7ac8290c6d5280
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-11 12:35:01 -0500

    Update grocery-app-homework.py

--- week-2/day-2/grocery-app-homework.py
+++ week-2/day-2/grocery-app-homework.py
@@ -1,3 +1,16 @@
+title = input("What's the name of store?")
+address = input("What's the address of the store?")
+
+class GroceryApp:
+    def __init__(self, title, address):
+        self.title = title
+        self.address = address
+
+    def add_a_store(self):
+        self.title = input("What's the name of the store?")
+        self.address = input("What's the address of the store?")
+
+    
 answer = ""
 while answer != "x":
     answer = input("""
@@ -7,31 +20,4 @@ while answer != "x":
     Press 3 to view lists\n
     Press x to exit.
     """)
-    if answer == "1":
-        storeadd = GroceryStoreApp.add_a_store()
-    elif answer == "2":
-        add_grocery_item
-
         
-
-
-
-class GroceryStoreApp:
-
-    def __init__(self):
-        pass
-          
-    def add_a_store(self):
-        self.title = input("What's the name of the store?")
-        self.address = input("What's the address of the store?")
-        self.grocery_title_list.append(self.title)
-        self.grocery_address_list.append(self.address)
-        self.grocery_stores_list.zip(grocery_title_list, grocery_address_list)
-
-    
-    def add_grocery_item(self):
-        self.item = input("What item would you like to add to your shopping list for %s?" % self.title)
-        self.price = input("What is the price of this item?\n")
-        self.quantity = input("How many of this item do you need?\n")
-    
-    
