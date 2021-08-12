26ff7e2f5091b0d9503ba516daeb2e50b8cf4a45
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-12 11:29:06 -0500

    Finished game!

--- week-2/day-3/game.py
+++ week-2/day-3/game.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 class Characters:
     def __init__(self, name, weapon, health=100, attack_power=10):
@@ -6,14 +7,30 @@ class Characters:
         self.weapon = weapon
         self.health = health
         self.attack_power = attack_power
+    
 
       
     def hero_lose_health(self):
-        self.health -= self.attack_power
+        self.health -= villain.attack_power
         print("Oh no! %s hit you with his %s! \nYour health is now %d." % (villain.name, villain.weapon, hero.health))
+        if hero.health == 0:
+            print("Oh look at that")
+            time.sleep(1)
+            print("You did what everyone thought you would do")
+            time.sleep(1)
+            print("\nAnd you died.\n")
+            time.sleep(1)
+            print("GOODBYE %s" % user_name)
+            
 
     def hero_runaway(self):
         print("You ran away.\nThat's so you %s" % user_name)
+        time.sleep(1)
+        print("I guess you saved yourself a few minutes...")
+        time.sleep(1)
+        print("You should really work on your cardio %s" % user_name)
+        time.sleep(1)
+
     
     def hero_eat_apples(self):
         if hero.health < 100:
@@ -21,37 +38,55 @@ class Characters:
             print("You've eaten apples to restore your health\nYour health is now %s." % self.health)
         elif hero.health >= 100:
             print("You don't need any apples\n\nTRUST me.\n")
-    
-    def defend_yourself(self):
-        print("You've defended the attack successfully!")
+            time.sleep(1)
 
 
     def deal_damage(self):
         villain.health -= hero.attack_power
         print("You've dealt %d damage to %s with your %s" % (self.attack_power, villain.name, hero.weapon))
+        print("%s's health is now: " % villain.name + str(villain.health))
 
         if villain.health == 0:
-            print("Oh my god\nWOW.\nJust WOW %s\nYou've defeated %s!\nI didn't think you were gonna survive, honestly." % (user_name, villain.name))
+            print("Oh my god")
+            time.sleep(1)
+            print("WOW")
+            time.sleep(1)
+            print("Just WOW %s" % user_name)
+            time.sleep(1)
+            print("You defeated %s!" % villain.name)
+            time.sleep(1)
+            print("You just got lucky. I'm sure you'll die next time")
+        
             user_choice == "x"
         
             
 
 
-villain = Characters("Joe Frasier", "Laser")
+villain = Characters("Joe Frasier", "Python", attack_power=15)
 print("Welcome to Hyrule Field!\n")
+time.sleep(1)
 
 user_name = input("What would you like your hero name to be?\n")
 if user_name == "":
     print("Ooooookay. I'm just gonna call you DumDum")
     user_name = "DumDum"
-
-print("Thats a terrible hero name.\n\n *Sigh*\n\n I guess it'll have to do.\n")
-
+time.sleep(1)
+print("Thats a terrible hero name.")
+time.sleep(1)
+print("\n\n *Sigh*\n\n")
+time.sleep(1)
+print("I guess it'll have to do.\n")
+time.sleep(1)
 user_weapon = input("What is your weapon of choice?\n")
+time.sleep(1)
+print("I mean...")
+time.sleep(1)
+print("you know what, whatever.\n\n")
+time.sleep(1)
 
-print("I mean...\nyou know what, whatever.\n\n")
+print("Good thing we have a\n **cough cough**\n\nHERO\n\nhere to save us, because a wild %s has appeared!!!" % villain.name)
+time.sleep(3)
 
-print("Good thing we have a\n **cough cough**\n\nHERO\n\nhere to save us, because a wild %s has appeared!" % villain.name)
 
 
 
@@ -73,10 +108,15 @@ hero = Characters(user_name, user_weapon)
 # villain.villain_lose_health()
 # print(hero.name)
 # print(villain.health)
-attack_chance = random.randint(1, 10)
 user_choice = ""
-while user_choice != "x" or user_choice != "X" or hero.health != 0 or villain.health != 0:
+while user_choice != "x" or user_choice != "X":
     
+    if villain.health == 0 or hero.health == 0:
+        user_choice == "x"
+        break
+
+    attack_chance = random.randint(1, 10)
+
     user_choice = input("""
     What would you like to do, %s?\n
     Press 1 to attack %s\n
@@ -85,38 +125,27 @@ while user_choice != "x" or user_choice != "X" or hero.health != 0 or villain.he
     Press x to exit, or if you're a weenie.\n 
     """ % (user_name, villain.name))
 
-    if villain.health == 0:
-        print("Just kidding, you killed the bad guy, go home %s" % user_name)
-        user_choice == "x"
-        break
 
-    if hero.health == 0:
-        print("You died.\nIt's ok.\n\nWe all expected it.")
-        print("Start over again %s" % user_name)
-        user_choice == "x"
-        break
-
-    elif hero.health == 0 and villain.health == 0:
+    if hero.health == 0 and villain.health == 0:
         print("Well now you've killed each other\nAt the same time\nThat's some Monty Python BS right there")
         user_choice == "x"
         break
 
     elif user_choice == "1":
         hero.deal_damage()
-        print("%s's health is now: " % villain.name + str(villain.health))
-        if attack_chance >= 6:
+        if attack_chance >= 6 and villain.health > 0:
             hero.hero_lose_health()
 
    
     elif user_choice == "2":
         hero.hero_runaway()
-        print("I guess you saved yourself a few minutes...\nYou should really work on your cardio %s" % user_name)
 
     elif user_choice == "3":
         hero.hero_eat_apples()
         
 
     elif user_choice == "x" or user_choice == "X":
+        print("Weenie.")
         break
     
     else: 
