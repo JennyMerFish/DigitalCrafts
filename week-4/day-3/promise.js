02146e42a23b4f25d32820049ad947b98191c871
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-25 13:55:11 -0500

    made help folder

--- week-4/day-3/promise.js
+++ week-4/day-3/promise.js
@@ -1,16 +1,18 @@
 async function getCatPics() {
     const imageContainer = document.querySelector(".catpics");
-    const catPics = await fetch("https://cataas.com/cat");
+    const catPics = await fetch("https://cataas.com/cat?json=true");
     const jsonCatPics = await catPics.json();
 
-    imageContainer.src = jsonCatPics.image;
-    
+    imageContainer.src = ("https://cataas.com" + jsonCatPics.url);
+    console.log(jsonCatPics);
     return jsonCatPics
 }
 
 
 const catButton = document.querySelector(".catbutton");
 catButton.addEventListener("click", () => getCatPics());
+
+
 
 
 
