3349e4fe56f7110ce2e241e1e8abff7e5366b601
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-08-27 11:34:39 -0500

    Update api.js

--- week-4/day-5/api.js
+++ week-4/day-5/api.js
@@ -5,13 +5,13 @@ async function getWeatherData() {
     const tempBox = document.querySelector(".temp");
     const skyBox = document.querySelector(".sky")
     const inputField = document.querySelector(".input-field");
-    const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&units=imperial&appid=750ea5847bbdbf22dec14f20d97e9f2e`)
+    const weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputField.value}&units=imperial&appid=`)
     const jsonWeatherData = await weatherData.json();
     const temp = jsonWeatherData.main.temp;
     const skyDescription = jsonWeatherData.weather[0].main;
     tempBox.innerHTML = (`The temperature outside is ${temp}\u00B0F`)
     skyBox.innerHTML = (`The sky is currently ${skyDescription}`)
-    
+
 
 
 
