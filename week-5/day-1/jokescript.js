77656cd4ec93abddf49e2621f9f2a323ab8c80dd
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-09-01 11:52:20 -0500

    Styling and layout on final pages

--- week-5/day-1/jokescript.js
+++ week-5/day-1/jokescript.js
@@ -1,12 +1,27 @@
+const body = document.querySelector(".clearingbox")
+
+
 async function getJokes(){
-    const joke = document.querySelector(".joke");
-    const punchline = document.querySelector(".punchline");
+    body.innerHTML = "";
+
+    const jokesBox = document.createElement("div");
+    jokesBox.className = "jokesBox"
+    
+    const joke = document.createElement("h3")
+    joke.className = "joke"
+    
+    const punchline = document.createElement("h3");
+    punchline.className = "punchline"
+    
     const jokeFetch = await fetch("https://official-joke-api.appspot.com/jokes/random");
     const jokeFetchJson = await jokeFetch.json()
-    joke.innerHTML = ""
-    punchline.innerhtml =""
+
     joke.innerHTML = jokeFetchJson.setup
     punchline.innerHTML = jokeFetchJson.punchline
+
+    jokesBox.append(joke, punchline)
+    body.append(jokesBox)
+
 }
 
 
