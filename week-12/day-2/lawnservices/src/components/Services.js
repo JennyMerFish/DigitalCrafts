be9bfc3d7aa8cf2dde0d73a9bea099d25916d892
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-11-01 09:02:37 -0500

    Finalizing CSS

--- week-12/day-2/lawnservices/src/components/Services.js
+++ week-12/day-2/lawnservices/src/components/Services.js
@@ -9,10 +9,13 @@ export default function Services() {
         <div className="servicesPage">
             <Link to="/">Back Home</Link>
             <h1 className="servicesHeader">Services</h1>
+            <div className = "servicesCards">
+
             {/* <img className="displayPhoto" src="https://images.unsplash.com/photo-1458245201577-fc8a130b8829?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80" alt="" /> */}
             {services?.map((service) => 
                 (<ServicesCards service={service}/>)
             )}
+            </div>
             
         </div>
     )
